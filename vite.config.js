f731444495252c908e2ceb41e179c9c15c07387a
Firdaus <firdausmustar@gmail.com>
2026-02-08 01:34:51 +0800

    update build for github page

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/formulari-ubat-pkdkl/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
